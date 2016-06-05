8cdb3650a3007466efc0ee130bd3f78a5ef2e181
Mark Steve Samson <hello@marksteve.com>
2016-06-05 23:12:37 +0800

    Add minimal syntax highlighting with microlight

--- renderers.py
+++ renderers.py
@@ -2,10 +2,18 @@ import mistune
 
 
 class DefaultRenderer(mistune.Renderer):
-    pass
+
+    def block_code(self, code, lang=None):
+        code = code.rstrip('\n')
+        if not lang:
+            code = mistune.escape(code, smart_amp=False)
+            return '<pre class="microlight"><code>%s\n</code></pre>\n' % code
+        code = mistune.escape(code, quote=True, smart_amp=False)
+        return '<pre class="microlight"><code class="lang-%s">%s\n</code></pre>\n' % (
+            lang, code)
 
 
-class _2014Renderer(mistune.Renderer):
+class _2014Renderer(DefaultRenderer):
 
     def header(self, text, level, raw=None):
         header = super(_2014Renderer, self).header(text, level, raw)
